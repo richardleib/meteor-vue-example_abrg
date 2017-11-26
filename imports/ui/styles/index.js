399b4ff0e659b80c16c58ac5710fc242e5efcabd
Richard Leibowitz <richardbbleibowitz@gmail.com>
2017-11-26 04:33:13 +0300

    - Add "seba:minifiers-autoprefixer" and "fourseven:scss" Meteor packages. - Add Bootstrap styles.

---
+++ imports/ui/styles/index.js
@@ -0,0 +1,5 @@
+// Import NPM here, because styles from NPM package is not available in stylesheets:
+// https://github.com/meteor/meteor/issues/6846
+import 'bootstrap/scss/bootstrap.scss'
+
+import './main.styl'
